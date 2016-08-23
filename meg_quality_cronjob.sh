c4bcdc51ae03ad0a77c51262a67f16185a557920
Robert Oostenveld <r.oostenveld@gmail.com>
2016-08-23 10:16:57 +0200

    modified the script to start the QC computation on the compute cluster rather than running it locally

--- meg_quality_cronjob.sh
+++ meg_quality_cronjob.sh
@@ -3,5 +3,10 @@
 # This script executes the "meg_quality_cronjob" script on the DCCN compute cluster.
 #
 
+# since this script is started from a cronjob, the path and environment variables are emtpy
+# set the correct path and ensure modules are available
+source /opt/optenv.sh
+module load matlab/R2012b
+
 cd /project/3010102.04/scripts/meg_quality
-matlab_sub meg_quality_cronjob.m
+/opt/cluster/bin/matlab_sub meg_quality_cronjob.m
