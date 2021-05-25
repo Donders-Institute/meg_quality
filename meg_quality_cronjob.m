e6b1bc5eff8dc456ac648985c743d1cb007c9f29
Robert Oostenveld <r.oostenveld@gmail.com>
2021-05-25 11:24:25 +0200

    updated the location of the raw data and results

--- meg_quality_cronjob.m
+++ meg_quality_cronjob.m
@@ -1,12 +1,13 @@
 function meg_quality_cronjob
 
-% MEM 8gb
+% MEM 12gb
 % WALLTIME 18:00:00
 
 % this function is running every night and computes some quality measures
 % for each of the new MEG datasets
 
 try
+  %%
   clear global
   
   global ft_default
@@ -20,13 +21,16 @@ try
   restoredefaultpath
   addpath /home/common/matlab/fieldtrip
   ft_defaults
-
+  
   addpath /project/3055020.02/code
-  cd /project/3055020.02/2020
   
-  % ensure that the data is read from the correct location
-  prefix  = '/project/3055020.01/raw/2020';
+  % this is where the raw data is located
+  prefix  = '/project/3055020.01/raw/2021';
   
+  % this is where the results will end up
+  cd /project/3055020.02/2021
+  
+  %%
   dataset = {};
   day     = dir(sprintf('%s/20*',prefix));
   day     = {day.name};
@@ -40,6 +44,7 @@ try
     dataset = cat(2, dataset, session);
   end
   
+  %%
   for i=1:length(dataset)
     
     % determine the exportname that ft_qualitycheck would have used on a previous run
@@ -78,4 +83,5 @@ catch ME
   end
 end
 
+disp('COMPLETED');
 exit
